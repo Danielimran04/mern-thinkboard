c20b3678e7173bf25a7ecaae29f88319e8e58b5b
MUHAMAD DANIEL IMRAN BIN ABU BAKAR <danielimranabubakar@gmail.com>
2026-08-08 16:02:30 +0800

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,16 +4,24 @@ import {connectDB} from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path"
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve()
 
 // middleware
-app.use(cors({
+
+if(process.env.NODE_ENV !== "production")
+{
+  app.use(cors({
   origin: "http://localhost:5173", // allow requests from this origin
 }));
+}
+
+
 
 app.use(express.json());// this middleware will parse the json body
 app.use(rateLimiter); // apply the rate limiter middleware to all routes
@@ -25,6 +33,15 @@ app.use(rateLimiter); // apply the rate limiter middleware to all routes
 // });
 
 app.use("/api/notes", notesRoutes);
+
+if(process.env.NODE_ENV === "production")
+{
+app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+app.get("*", (req,res) => {
+  res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+});
+}
 
 
 
